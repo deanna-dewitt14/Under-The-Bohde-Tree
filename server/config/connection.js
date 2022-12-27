33ab14cc11c09f66d543b8684e699114f9520b2c
BRIAN LOCKHART <bslockhart@uncg.edu>
2022-12-27 15:01:34 -0500

    updated files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://192.168.1.11:3000/under-the-bohde-tree", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://192.168.1.11:3000/brians-book-search-engine", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
