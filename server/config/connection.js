5b384b3677b4b7f8c6bfda64e35aa641818d9f67
BRIAN LOCKHART <bslockhart@uncg.edu>
2022-12-27 13:58:42 -0500

    .json updated bycript

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://192.168.1.11:3000/Under-The-Bohde-Tree", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://192.168.1.11:3000/under-The-the-bohde-tree", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
