228e4597d4fcf88b256dbaa56ad46eaf89533de1
BenS213 <57325318+BenS213@users.noreply.github.com>
2023-01-11 18:23:04 -0500

    removed second username

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -152,7 +152,6 @@ const Profile = () => {
               <h1 className="text-4xl font-semibold drop-shadow">
                 {userData.username}
               </h1>
-              <p className="font-medium text-gray-100">{userData.username}</p>
             </div>
           </div>
           <div className="justify-stretch mt-6 flex flex-col-reverse space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-y-0 sm:space-x-3 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
